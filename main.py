5afb1d4ce6d29d4f297ce5202187da1381ec8810
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-07-02 08:58:15 +0300

    Adding chromedriver

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from selenium import webdriver
+import time
 
-driver = webdriver.Chrome()
-driver.get("https://www.google.com/")
+chrome_driver_path = "/Users/dsannikov/Documents/GitHub/ParsingPages/telegram-selenium-head_hunter/driver/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+url = "https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&ored_clusters=true&search_field=name&search_period=1&text=%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82+python&from=SIMILAR_QUERY&hhtmFromLabel=SIMILAR_QUERY&hhtmFrom=vacancy_search_list"
 
-driver.quit()
+
+try:
+    driver.get(url=url)
+    time.sleep(30)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
