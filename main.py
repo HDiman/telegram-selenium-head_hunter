ff19b2f2e5122c2719488737a585b07cf53f9169
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-07-02 21:57:01 +0300

    Selenium Wire

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from selenium import webdriver
+# from selenium import webdriver
+from seleniumwire import webdriver
 import time
 from fake_useragent import UserAgent
-import random
+
 
 chrome_driver_path = "/Users/dsannikov/Documents/GitHub/ParsingPages/telegram-selenium-head_hunter/driver/chromedriver"
 
@@ -12,12 +13,17 @@ useragent = UserAgent(verify_ssl=False)
 options = webdriver.ChromeOptions()
 options.add_argument(f"user-agent={useragent.random}")
 
+# set proxy
+options.add_argument("--proxy-server=20.47.108.204:8888")
+
 
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
 
 
 # url = "https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&ored_clusters=true&search_field=name&search_period=1&text=%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82+python&from=SIMILAR_QUERY&hhtmFromLabel=SIMILAR_QUERY&hhtmFrom=vacancy_search_list"
-url = "https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
+# url = "https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
+url = "https://2ip.ru"
+
 
 try:
     driver.get(url=url)
