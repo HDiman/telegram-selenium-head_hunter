c9f901d668d22305a3072ddb4882712a6ead3a8d
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-07-06 21:00:28 +0300

    Selenium Wire

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ options = webdriver.ChromeOptions()
 options.add_argument(f"user-agent={useragent.random}")
 
 # set proxy
-options.add_argument("--proxy-server=20.47.108.204:8888")
+options.add_argument("--proxy-server=23.227.38.25:80")
 
 
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
@@ -22,12 +22,12 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
 
 # url = "https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&ored_clusters=true&search_field=name&search_period=1&text=%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82+python&from=SIMILAR_QUERY&hhtmFromLabel=SIMILAR_QUERY&hhtmFrom=vacancy_search_list"
 # url = "https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
-url = "https://2ip.ru"
-
+# url = "https://2ip.ru"
+url = "https://profi.ru/backoffice/n.php"
 
 try:
     driver.get(url=url)
-    time.sleep(10)
+    time.sleep(100)
 except Exception as ex:
     print(ex)
 finally:
