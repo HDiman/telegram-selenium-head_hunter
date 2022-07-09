d86142e5b4cefbd1b3ff7b12de01507a7d4843b8
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-07-10 01:56:37 +0300

    Entering job done section

--- main.py
+++ main.py
@@ -4,72 +4,68 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import time
 
+# Path to chromedriver
+chrome_driver_path = "/Users/dsannikov/Documents/GitHub/ParsingPages/telegram-selenium-head_hunter/driver/chromedriver"
 
-# options
+# Options
 options = webdriver.ChromeOptions()
 options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36")
-chrome_driver_path = "/Users/dsannikov/Documents/GitHub/ParsingPages/telegram-selenium-head_hunter/driver/chromedriver"
+
+# Creating Driver Object
 driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
 
-
-
+# Link to Main Webpage
 url = "https://profi.ru/backoffice/n.php"
-# url = "https://vk.com/"
-# url = "https://id.vk.com/auth?app_id=7913379&v=1.48.0&redirect_uri=https%3A%2F%2Fvk.com%2Ffeed&uuid=vlYZgdUAB-3jnb4qY5xlk&action=eyJuYW1lIjoibm9fcGFzc3dvcmRfZmxvdyIsInBhcmFtcyI6eyJ0eXBlIjoic2lnbl9pbiJ9fQ%3D%3D"
-# url = "https://phptravels.com/demo/"
 
-
+# Entering inside the Main page
 try:
+    # Enter page
     driver.get(url=url)
     time.sleep(5)
 
+    # Enter login with phone number
     login_input = driver.find_element(By.CLASS_NAME, "login-form__input-login")
     login_input.clear()
     login_input.send_keys("+79160585921")
     time.sleep(5)
-
     submit_button = driver.find_element(By.CLASS_NAME, "login-form__button").click()
+
+    # Input code instead of password
     input_code = input("Please input number: ")
     time.sleep(5)
-
     for i in range(4):
         enter_code = driver.find_element(By.CLASS_NAME, "ui-pin-input")
         enter_code.send_keys(f"{input_code[i]}")
         time.sleep(2)
+
+    # Waiting for page downloaded
     time.sleep(60)
 
-    # //*[@id="BO_REACT_MOBILE_TAB_BAR"]/nav/a[2]
+    # Find out href link to pass for enter inside
     chat_order = driver.find_element(By.XPATH, "//*[@id='BO_REACT_MOBILE_TAB_BAR']/nav/a[2]").get_attribute('href')
     print(chat_order)
     time.sleep(5)
 
     # inside chat
+    driver.get(url=chat_order)
+    time.sleep(5)
+
+    # Find out href link to pass for Finished Jobs
+    finished_order = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/a[3]").get_attribute('href')
+    print(finished_order)
+    time.sleep(5)
+
+    # inside chat
+    driver.get(url=finished_order)
+    time.sleep(5)
+
+    # Subjects
+    subjects_fin_order = driver.find_elements(By.CLASS_NAME, "subjects")
+    for i in range(len(subjects_fin_order)):
+        print(f"{i+1}: {subjects_fin_order[i].text}")
 
 
-    # # Title of the order
-    # title_order = driver.find_element(By.CLASS_NAME, "subjects.viewport-marker").text
-    # print(title_order)
-    # time.sleep(5)
-
-    # # Order description
-    # desc_order = driver.find_element(By.CLASS_NAME, "aim").text
-    # print(desc_order)
-    # time.sleep(5)
-    #
-    # # Client info
-    # client_order = driver.find_element(By.CLASS_NAME, "client-info.order_block__client-info").text
-    # print(client_order)
-    # time.sleep(5)
-    #
-    # # Address
-    # address_order = driver.find_element(By.XPATH, '//*[@id="order_block_49212293"]/tbody/tr/td[2]/div[2]').text
-    # print(address_order)
-    # time.sleep(5)
-
-    # //*[@id="order_block_49216876"]/tbody/tr/td[2]/div[2]
-    # //*[@id="order_block_49212293"]/tbody/tr/td[2]/div[2]
-
-    time.sleep(60)
+    time.sleep(600)
 
 
 
